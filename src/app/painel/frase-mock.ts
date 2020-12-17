1fab535b5c36559180c5bf67ec788cd1ef1ed5eb
Debora <debribeirs@gmail.com>
2020-12-17 07:07:57 -0300

    initial commit

---
+++ src/app/painel/frase-mock.ts
@@ -0,0 +1,9 @@
+import { Frase } from '../shared/frase.model'
+
+export const Frases: Frase[] = [
+    {fraseEng: 'How are you?', frasePTBr: 'Como vai você?'},
+    {fraseEng: 'I like to learn', frasePTBr: 'Eu gosto de aprender'},
+    {fraseEng: 'I am learning Angular', frasePTBr: 'Estou aprendendo Angular'},
+    {fraseEng: 'I am learning Typescript', frasePTBr: 'Estou aprendendo Typescript'},
+
+]
---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,12 @@
+export class Frase{
+
+    //Otimização do código - refactore
+    //constructor(public fraseEng: string, public frasePTBr: string){
+    //    this.fraseEng = fraseEng
+    //    this.frasePTBr = frasePTBr
+    //}
+    //refactore - no próprio construtor criamos os atributos da classe
+    constructor(public fraseEng: string, public frasePTBr: string){}
+
+    
+}
